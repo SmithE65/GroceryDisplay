d83020a895cc890e02fbe164bea66c2758683955
Eric Smith <ydrive65@yahoo.com>
2026-07-04 11:12:26 -0400

    start adding pg backend

--- GroceryDisplay.AppHost/AppHost.cs
+++ GroceryDisplay.AppHost/AppHost.cs
@@ -1,5 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.GroceryDisplay_Api>("grocerydisplay-api");
+var groceryPostgres = builder.AddPostgres("grocerydb-instance")
+    .WithPgAdmin();
+
+var groceryDb = groceryPostgres.AddDatabase("grocerydb");
+
+builder.AddProject<Projects.GroceryDisplay_Api>("grocerydisplay-api")
+    .WithReference(groceryDb); ;
 
 builder.Build().Run();
